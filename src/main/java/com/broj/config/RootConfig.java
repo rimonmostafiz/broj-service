75ae227c7c779a0f6f69883800ab4ba128ea13fa
Muztaba Hasanat <hasanat.muztaba@gmail.com>
2016-07-21 17:39:26 +0600

    init repo

---
+++ src/main/java/com/broj/config/RootConfig.java
@@ -0,0 +1,11 @@
+package com.broj.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by LynAs on 23-Jan-16
+ */
+@Configuration
+@ComponentScan(basePackages={"com.broj"})
+public class RootConfig {}
---
+++ src/main/java/com/broj/config/WebConfig.java
@@ -0,0 +1,81 @@
+package com.broj.config;
+
+import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.broj")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+    public ServletContextTemplateResolver templateResolverDev() {
+        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+        resolver.setCacheable(false);
+        setProperty(resolver);
+        return resolver;
+    }
+
+    public void setProperty(ServletContextTemplateResolver resolver) {
+        resolver.setPrefix("/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        Set<IDialect> dialects = new HashSet<>();
+        dialects.add(new LayoutDialect());
+
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolverDev());
+        engine.setAdditionalDialects(dialects);
+        engine.addDialect(new DataAttributeDialect());
+        return engine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setOrder(1);
+        resolver.setViewNames(new String[]{"*", "js/*", "template/*"});
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
