426833183fca0a9687f0807c3fc5338695df30b2
seal <hasanat.muztaba@gmail.com>
2016-07-28 00:28:50 +0600

    useful method for path and file name added. path related utility method placed in model class

--- src/main/java/com/broj/model/CompileRequest.java
+++ src/main/java/com/broj/model/CompileRequest.java
@@ -1,5 +1,6 @@
 package com.broj.model;
 
+import com.broj.util.PathUtil;
 import lombok.*;
 
 /**
@@ -24,4 +25,33 @@ public class CompileRequest {
 
     @Setter(AccessLevel.PRIVATE)
     private String outputFilePath;
+
+    @Setter(AccessLevel.PRIVATE)
+    private long timeLimit;
+
+    @Setter(AccessLevel.PRIVATE)
+    private String fileName; // like A.java
+
+    @Setter(AccessLevel.PRIVATE)
+    private String fileNameWithoutExtension; // like A
+
+    @Setter(AccessLevel.PRIVATE)
+    private String parentFilePath;
+
+    public String getFileName() {
+        return fileName != null ? fileName :
+                (fileName = PathUtil.fileNameFromPath(sourceFilePath));
+    }
+
+    public String getFileNameWithoutExtension() {
+        if (fileNameWithoutExtension == null) {
+            String[] strings = getFileName().split("\\.");
+            fileNameWithoutExtension = strings[0];
+        }
+        return fileNameWithoutExtension;
+    }
+
+    public String getParentFilePath() {
+        return parentFilePath != null ? parentFilePath : (parentFilePath = PathUtil.filePath(sourceFilePath));
+    }
 }
